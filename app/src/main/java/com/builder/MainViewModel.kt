77f2d7216b51767a64b9c45950bc85e8037cc3a7
invuzt <invuzt@gmail.com>
2026-05-18 11:56:57 +0000

    fix: typo AsStateFlow to asStateFlow in MainViewModel

--- app/src/main/java/com/builder/MainViewModel.kt
+++ app/src/main/java/com/builder/MainViewModel.kt
@@ -3,11 +3,12 @@ package com.builder
 import android.graphics.Bitmap
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.AsStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class MainViewModel: ViewModel() {
     private val _bitmaps = MutableStateFlow<List<Bitmap>>(emptyList())
-    val bitmaps = _bitmaps.AsStateFlow()
+    // Diperbaiki menggunakan huruf kecil di awal: asStateFlow()
+    val bitmaps = _bitmaps.asStateFlow()
 
     fun onTakePhoto(bitmap: Bitmap) {
         _bitmaps.value = _bitmaps.value + bitmap
